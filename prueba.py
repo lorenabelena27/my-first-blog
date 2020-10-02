cf10ba3506843c4b4566aee3ce0812071970748b
lorena <lorenabelena@gmail.com>
2020-10-02 15:06:17 +0200

    My Django Girls app,First commit

---
+++ prueba.py
@@ -0,0 +1,17 @@
+def operaciones(num1,num2):
+	resultado=num1+num2
+	return resultado
+
+
+print(operaciones(2,6))
+
+def hi(name):
+	print('Hi '+ name +'!')
+
+hi('ld')
+
+girls = ['Raquel','Monica','Lucia']
+
+for name in girls:
+	hi(name)
+	print('Next girl')
